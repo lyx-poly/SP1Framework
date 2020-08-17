7c00539e95fd603556779a2e1bff546c320da236
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-17 12:53:08 +0800

    Enemy Class Update
    Defined enemy class.

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -1,1 +1,35 @@
 #include "Enemy.h"
+
+Enemy::Enemy()
+{
+	//
+}
+
+void Enemy::Define(int hp, int dmg)
+{
+	HP = hp;
+	Damage = dmg;
+}
+
+void Enemy::movement()
+{
+}
+
+void Enemy::Attack(Entity* ptrEntity)
+{
+}
+
+void Enemy::Defend()
+{
+}
+
+void Enemy::sethealth(int health)
+{
+	Hp = health;
+	return 0;
+}
+
+Enemy::~Enemy()
+{
+	//
+}
--- SP1Framework/Enemy.h
+++ SP1Framework/Enemy.h
@@ -1,5 +1,13 @@
 #pragma once
-class Enemy
+#include "Entity.h"
+class Enemy : class Entity
 {
+public:
+	Enemy();
+	void Define(int hp, int dmg); // Sets stats to an enemy object
+	void movement(); // Movement AI
+	void Attack(Entity* ptrEntity);
+	void Defend();
+	~Enemy();
 };
 
