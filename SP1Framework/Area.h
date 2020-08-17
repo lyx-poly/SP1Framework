98c21e9bb3d63abd5324a23aed81b29574561639
Joashfeh <63983498+Joashfeh@users.noreply.github.com>
2020-08-17 10:24:45 +0800

    Map Generation

--- SP1Framework/Area.h
+++ SP1Framework/Area.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <iostream>
+
 class Area {
+public:
+	int area[16][16];
+	unsigned short flags;
+
+	Area();
+	Area(unsigned short flags, int doorSize, int roomType);
+
+	void display();
 };
 
--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -1,5 +1,18 @@
 #pragma once
-class Map
-{
+#include "Area.h"
+#include <stdlib.h>
+#include <time.h>
+
+class Map {
+public:
+	int floor;
+	Area* map[6][6]; // 4x4 map array but taken as 6x6 for camera tracking
+	int toDisplay[96][192]; // upscaled to display onto the screen ( 2 x 4 )
+
+	Map(int f, int floorType); // floorType: 1 for normal dungeons, 2 for bosses
+	~Map();
+
+	void generateDungeon();
+
 };
 
